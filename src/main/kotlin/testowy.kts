7b3926f04fa4cf3d92e346cdb7beb58e7be151c3
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2021-12-14 16:04:00 +0100

    operatory, zmienne, konwersja zmiennych, if-else

--- src/main/kotlin/testowy.kts
+++ src/main/kotlin/testowy.kts
@@ -1,6 +1,5 @@
-// https://www.w3schools.com/kotlin/kotlin_variables.php
-
-fun main(args : Array<String>){
+//fun main(args : Array<String>){
+fun main(){
     println("Hello world - main")
     print("Nie przechodzimy do nastepnej linii. ")
     // komentarz jednoliniowy
@@ -15,9 +14,8 @@ fun main(args : Array<String>){
      * */
     println("name: $name, date: $date")
     name = "Jacob"
-    var surname: String = "Michael"
-    surname = "Jordan"
-    println(surname)
+    var surname: String = "Jordan"
+    println("$surname $name")
 
     println("Typy danych: ")
     val myNum : Int = 5
@@ -25,6 +23,35 @@ fun main(args : Array<String>){
     val myLetter : Char = 'D'
     val myBoolean : Boolean = true
     val myText : String = "Hello"
+    println("myNum: $myNum, myDoubleNum: $myDoubleNum, myLetter: $myLetter, myBoolean: $myBoolean, myText: $myText")
 
+    val num1 : Byte = 100
+    val num2 : Short = 5000
+    val num3 : Int = 100000
+    val num4 : Long = 15000000000L
+    println("num1: $num1, num2: $num2, num3: $num3, num4: $num4")
+
+    val myFloat: Float = 5.75F
+    val myDouble: Double = 19.99
+
+    println("$myFloat ~ $myDouble")
+    println("Float ma precyzje szesciu-siedmu cyfr, a double pietnastu. ")
+
+    val x : Int = 5
+    val xLong : Long = x.toLong()
+    val xByte : Byte = x.toByte()
+    val xShort : Short = x.toShort()
+
+    println("Operatory: +, -, *, /, %, ++, -- ")
+    println("Operatory przypisania: =, +=, *=, /=, %= ")
+    println("Operatory porównania: ==, !=, >, <, >=, <=")
+    println("Operatory logiczne: &&, ||, !")
+
+    if (x == xLong.toInt()){
+        println("Sa rowne!")
+
+    } else {
+        println("Nie sa rowne!")
+    }
 
 }
