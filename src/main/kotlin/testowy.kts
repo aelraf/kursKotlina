d041b889ac5a28001b2eab7815cc1af2287122dd
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2021-12-14 13:42:17 +0100

    kursKotlina - zmienne i println

---
+++ src/main/kotlin/testowy.kts
@@ -0,0 +1,17 @@
+// https://www.w3schools.com/kotlin/kotlin_variables.php
+
+fun main(args : Array<String>){
+    pritnln("Hello world - main")
+    print("Nie przechodzimy do nastepnej linii. ")
+    // komentarz jednoliniowy
+    pritnln("Natomiast println zawiera znak konca wiersza. ")
+    /* komentarz
+    * wieloliniowy */
+
+    var name = "John"
+    val date = 1975
+    /*  pierwsza z nich moze zmieniac przechowywane wartosci,
+     * natomiast druga jest stala.
+     * */
+
+}
