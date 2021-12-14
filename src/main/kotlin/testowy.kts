37ee8d1cdd5cfa1677e5941c800309264682f27d
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2021-12-14 14:42:50 +0100

    Typy danych i wyświetlanie

--- src/main/kotlin/testowy.kts
+++ src/main/kotlin/testowy.kts
@@ -1,10 +1,10 @@
 // https://www.w3schools.com/kotlin/kotlin_variables.php
 
 fun main(args : Array<String>){
-    pritnln("Hello world - main")
+    println("Hello world - main")
     print("Nie przechodzimy do nastepnej linii. ")
     // komentarz jednoliniowy
-    pritnln("Natomiast println zawiera znak konca wiersza. ")
+    println("Natomiast println zawiera znak konca wiersza. ")
     /* komentarz
     * wieloliniowy */
 
@@ -13,5 +13,18 @@ fun main(args : Array<String>){
     /*  pierwsza z nich moze zmieniac przechowywane wartosci,
      * natomiast druga jest stala.
      * */
+    println("name: $name, date: $date")
+    name = "Jacob"
+    var surname: String = "Michael"
+    surname = "Jordan"
+    println(surname)
+
+    println("Typy danych: ")
+    val myNum : Int = 5
+    val myDoubleNum : Double = 5.99
+    val myLetter : Char = 'D'
+    val myBoolean : Boolean = true
+    val myText : String = "Hello"
+
 
 }
