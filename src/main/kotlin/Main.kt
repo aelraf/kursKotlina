ec22f29e895ebf14265d122835eccf91445ca7e6
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2021-12-15 20:54:55 +0100

    klasy, metody klasowe

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,6 +5,18 @@ class Car {
 }
 
 
+class Auto(var brand: String, var model: String, var year: Int){
+    fun drive(){
+        println("Wroooom!")
+    }
+
+    fun speed(maxSpeed: Int){
+        println("Max speed is: $maxSpeed")
+    }
+
+}
+
+
 fun main(args: Array<String>) {
     println("Hello World!")
 
@@ -14,5 +26,18 @@ fun main(args: Array<String>) {
 
     val c1 = Car()
 
+    c1.brand = "Ford"
+    c1.model = "Mustang"
+    c1.year = 1970
+
+    println("brand: ${c1.brand}, model: ${c1.model}, year: ${c1.year}")
+
+    val a1 = Auto("BMW", "X5", 1999)
+    val a2 = Auto("Mercedes", "S", 2020)
+    val a3 = Auto("Tesla", "Model S", 2020)
+
+    a1.drive()
+    a1.speed(200)
 
 }
+
